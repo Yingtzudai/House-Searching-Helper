05e4027272c7ad04f8b4e52b3c084d5b7ccce416
Ying <yingtzu212@gmail.com>
2024-11-30 11:28:36 +0100

    Update Scrapy Items

--- housescraper/housescraper/items.py
+++ housescraper/housescraper/items.py
@@ -6,7 +6,37 @@
 import scrapy
 
 
+
+
+
 class HousescraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    house_name = scrapy.Field()
+    address = scrapy.Field()
+    price = scrapy.Field()
+    deposit = scrapy.Field()
+    service_cost = scrapy.Field()
+    area = scrapy.Field()
+    number_of_rooms = scrapy.Field()
+    interior = scrapy.Field()
+    dwelling_type = scrapy.Field()
+    property_type = scrapy.Field()
+    construction_type = scrapy.Field()
+    year_of_construction = scrapy.Field()
+    number_of_bedrooms = scrapy.Field()
+    number_of_bathrooms = scrapy.Field()
+    balcony = scrapy.Field()
+    garden = scrapy.Field()
+    energy_rating = scrapy.Field()
+    smoking_allowed = scrapy.Field()
+    pets_allowed = scrapy.Field()
+    offered_since = scrapy.Field()
+    status = scrapy.Field()
+    available = scrapy.Field()
+    rental_agreement = scrapy.Field()
+    duration = scrapy.Field()
+    agent_name = scrapy.Field()
+    agent_url = scrapy.Field()
+    house_url = scrapy.Field()
+    description = scrapy.Field()
--- housescraper/housescraper/spiders/housespider.py
+++ housescraper/housescraper/spiders/housespider.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from housescraper.items import HousescraperItem
 
 class HousespiderSpider(scrapy.Spider):
     name = "housespider"
@@ -25,6 +25,7 @@ class HousespiderSpider(scrapy.Spider):
             yield response.follow(next_page_url, callback = self.parse)
     
     def parse_house_page(self, response):
+        house_item = HousescraperItem()
         house = response.css('div.listing-detail-summary')
         transfer = response.css('section.page__details--transfer') 
         construction = response.css('section.page__details--construction')
@@ -35,36 +36,35 @@ class HousespiderSpider(scrapy.Spider):
         agent = response.css('section.agent-summary')
         agent_page = 'https://www.pararius.com' + 'a.agent-summary__title-link::attr(href)'
 
-        yield{
-            'House Name': house.css('h1.listing-detail-summary__title::text').get(),
-            'Address': house.css('div.listing-detail-summary__location::text').get(),
-            'Price': house.css('span.listing-detail-summary__price-main::text').get().strip(), # Remove Euros sign
-            # 'Deposite': transfer.css('dd.listing-features__description--deposit span.listing-features__main-description::text').get(),
-            # 'Service Cost': transfer.css('ul.listing-features__sub-description li::text').get(),
-            'Area': house.css('li.illustrated-features__item--surface-area::text').get().strip(),
-            'Number of Rooms': house.css('li.illustrated-features__item--number-of-rooms::text').get(), # Strip if not None
-            'Interior': house.css('li.illustrated-features__item--interior::text').get(), # strip if not None
-            'Dewlling Type': construction.css('dd.listing-features__description--dwelling_type span.listing-features__main-description::text').get(),
-            'Property Type': construction.css('dd.listing-features__description--property_types span.listing-features__main-description::text').get(),
-            'Construction Type': construction.css('dd.listing-features__description--construction_type span.listing-features__main-description::text').get(),
-            'Year of Construction': construction.css('dd.listing-features__description--construction_period span.listing-features__main-description::text').get(), # Change to integer
-            'Number of Rooms': layout.css('dd.listing-features__description--number_of_rooms span.listing-features__main-description::text').get(),
-            'Number of Bedrooms': layout.css('dd.listing-features__description--number_of_bedrooms span.listing-features__main-description::text').get(),
-            'Number of Bathrooms': layout.css('dd.listing-features__description--number_of_bathrooms span.listing-features__main-description::text').get(),
-            'Balcony': outdoor.css('dd.listing-features__description--balcony span.listing-features__main-description::text').get(),
-            'Garden': outdoor.css('dd.listing-features__description--garden span.listing-features__main-description::text').get(),
-            'Energy Rating': energy.css('dt:contains("Energy rating") + dd span.listing-features__main-description::text').get().strip(),
-            'Smoking Allowed': condition.css('dd.listing-features__description--smoking_allowed span.listing-features__main-description::text').get(),
-            'Pets Allowed': condition.css('dd.listing-features__description--pets_allowed span.listing-features__main-description::text').get(),
-            'Offered Since': transfer.css('dd.listing-features__description--offered_since span.listing-features__main-description::text').get(), # Change to date
-            'Status': transfer.css('dd.listing-features__description--status span.listing-features__main-description::text').get(),
-            'Available': transfer.css('dd.listing-features__description--acceptance span.listing-features__main-description::text').get(), # change to date
-            'Rental Agreement': transfer.css('dd.listing-features__description--contract_duration span.listing-features__main-description::text').get(),
-            'Duration':  transfer.css('dd.listing-features__description--contract_duration_min_max span.listing-features__main-description::text').get(),
-            'Agent Name': agent.css('a.agent-summary__title-link::text').get(),
-            'Agent URL': agent_page,
-            'House URL': response.css("link[rel='alternate'][hreflang='en']::attr(href)").get(),
-            'Description': response.css('div.listing-detail-description__additional *::text').getall()
+        house_item['House Name']= house.css('h1.listing-detail-summary__title::text').get()
+        house_item['Address']= house.css('div.listing-detail-summary__location::text').get()
+        house_item['Price']= house.css('span.listing-detail-summary__price-main::text').get() # Remove Euros sign and comma
+        house_item['Deposite']= transfer.css('dd.listing-features__description--deposit span.listing-features__main-description::text').get() # Remove Euros sign and comma
+        house_item['Service Cost']= transfer.css('ul.listing-features__sub-description li::text').get()
+        house_item['Area']= house.css('li.illustrated-features__item--surface-area::text').get()
+        house_item['Number of Rooms']= house.css('li.illustrated-features__item--number-of-rooms::text').get() # Strip if not None
+        house_item['Interior']= house.css('li.illustrated-features__item--interior::text').get() # strip if not None
+        house_item['Dewlling Type']= construction.css('dd.listing-features__description--dwelling_type span.listing-features__main-description::text').get()
+        house_item['Property Type']= construction.css('dd.listing-features__description--property_types span.listing-features__main-description::text').get()
+        house_item['Construction Type']= construction.css('dd.listing-features__description--construction_type span.listing-features__main-description::text').get()
+        house_item['Year of Construction']= construction.css('dd.listing-features__description--construction_period span.listing-features__main-description::text').get() # Change to integer
+        house_item['Number of Rooms']= layout.css('dd.listing-features__description--number_of_rooms span.listing-features__main-description::text').get()
+        house_item['Number of Bedrooms']= layout.css('dd.listing-features__description--number_of_bedrooms span.listing-features__main-description::text').get() # Change to integer
+        house_item['Number of Bathrooms']= layout.css('dd.listing-features__description--number_of_bathrooms span.listing-features__main-description::text').get() # Change to integer
+        house_item['Balcony']= outdoor.css('dd.listing-features__description--balcony span.listing-features__main-description::text').get()
+        house_item['Garden']= outdoor.css('dd.listing-features__description--garden span.listing-features__main-description::text').get()
+        house_item['Energy Rating']= energy.css('dt:contains("Energy rating") + dd span.listing-features__main-description::text').get()
+        house_item['Smoking Allowed']= condition.css('dd.listing-features__description--smoking_allowed span.listing-features__main-description::text').get()
+        house_item['Pets Allowed']= condition.css('dd.listing-features__description--pets_allowed span.listing-features__main-description::text').get()
+        house_item['Offered Since']= transfer.css('dd.listing-features__description--offered_since span.listing-features__main-description::text').get() # Change to date
+        house_item['Status']= transfer.css('dd.listing-features__description--status span.listing-features__main-description::text').get()
+        house_item['Available']= transfer.css('dd.listing-features__description--acceptance span.listing-features__main-description::text').get() # change to date
+        house_item['Rental Agreement']= transfer.css('dd.listing-features__description--contract_duration span.listing-features__main-description::text').get()
+        house_item['Duration']=  transfer.css('dd.listing-features__description--contract_duration_min_max span.listing-features__main-description::text').get()
+        house_item['Agent Name']= agent.css('a.agent-summary__title-link::text').get()
+        house_item['Agent URL']= agent_page
+        house_item['House URL']= response.css("link[rel='alternate'][hreflang='en']::attr(href)").get()
+        house_item['Description']= response.css('div.listing-detail-description__additional *::text').getall()
+        yield house_item
 
-        }
 
