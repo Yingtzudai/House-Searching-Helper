cd949cea5689d068f6466bb4b0e21e1ac9b9e990
Ying <yingtzu212@gmail.com>
2024-12-07 19:34:42 +0100

    Created new tables and dashboard

--- housescraper/housedata transformation.sql
+++ housescraper/housedata transformation.sql
@@ -2,9 +2,8 @@ use house;
 
 -- Create agent table
 CREATE TABLE IF NOT EXISTS agent (
-agent_id INT AUTO_INCREMENT PRIMARY KEY,
 agent_name VARCHAR(255) NOT NULL,
-agent_url VARCHAR(255) NOT NULL UNIQUE
+agent_url VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY
 );
 INSERT INTO agent(agent_url, agent_name)
 SELECT DISTINCT agent_url, agent_name
@@ -20,4 +19,64 @@ INSERT INTO territory(city, district)
 SELECT DISTINCT city, district
 FROM house;
 
--- Create date table
+-- Transform territory and house tables
+SET SQL_SAFE_UPDATES = 0;
+ALTER TABLE house
+ADD COLUMN region VARCHAR(255);
+UPDATE house
+SET region = CONCAT(district, ', ', city);
+
+ALTER TABLE territory
+ADD COLUMN region VARCHAR(255);
+UPDATE territory
+SET region = CONCAT(district, ', ', city);
+ALTER TABLE territory ADD PRIMARY KEY (region);
+SET SQL_SAFE_UPDATES = 1;
+
+-- Create house_overview table
+CREATE TABLE IF NOT EXISTS house_overview (
+                         house_name text,
+                         house_url VARCHAR(255) PRIMARY KEY,
+                         region VARCHAR(255),
+                         dwelling_type text,
+                         construction_type text,
+                         year_of_construction INTEGER,
+                         balcony text,
+                         garden text,
+                         number_of_bathrooms INTEGER,
+                         number_of_bedrooms INTEGER,
+                         number_of_rooms INTEGER,
+                         living_area_m2 DECIMAL,
+                         interior text,
+                         energy_rating VARCHAR(255),
+                         pets_allowed text,
+                         smoking_allowed text,
+                         offered_since VARCHAR(255),
+                         available VARCHAR(255),
+                         minimum_months INTEGER,
+                         maximum_months INTEGER,
+                         rental_agreement text,
+                         price DECIMAL,
+                         deposit DECIMAL,
+                         service_cost text,
+                         status text,
+                         agent_url VARCHAR(255),
+                         FOREIGN KEY (agent_url) REFERENCES agent(agent_url),
+                         FOREIGN KEY (region) REFERENCES territory(region)
+                         
+);
+
+INSERT INTO house_overview
+SELECT house_name, house_url, region, dwelling_type, construction_type, year_of_construction, balcony, garden,
+number_of_bathrooms, number_of_bedrooms, number_of_rooms, living_area_m2, interior, energy_rating, pets_allowed, 
+smoking_allowed, offered_since, available, minimum_months, maximum_months, rental_agreement, price, deposit, 
+service_cost, status, agent_url
+FROM house;
+
+
+
+
+
+
+
+
