2869db3d83e54d4ab7eddb765e9ba81d0c7ec7c6
Ying <yingtzu212@gmail.com>
2024-12-11 18:10:38 +0100

    Transformed data

--- housescraper/housedata transformation.sql
+++ housescraper/housedata transformation.sql
@@ -8,30 +8,24 @@ CREATE TABLE IF NOT EXISTS agent (
 agent_name VARCHAR(255) NOT NULL,
 agent_url VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY
 );
-INSERT INTO agent(agent_url, agent_name)
+INSERT IGNORE INTO agent(agent_url, agent_name)
 SELECT DISTINCT agent_url, agent_name
 FROM house;
 
 -- Create territory table
 CREATE TABLE IF NOT EXISTS territory (
 city VARCHAR(255) NOT NULL,
-district VARCHAR(255) NOT NULL
+district VARCHAR(255) NOT NULL,
+region VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY
 );
 
-INSERT INTO territory(city, district)
-SELECT DISTINCT city, district
+INSERT IGNORE INTO territory(city, district, region)
+SELECT city, district, CONCAT(district, ', ', city) AS region
 FROM house;
-
--- Transform territory table
-ALTER TABLE territory
-ADD COLUMN region VARCHAR(255);
-UPDATE territory
-SET region = CONCAT(district, ', ', city);
-ALTER TABLE territory ADD PRIMARY KEY (region);
 
 -- Create house_info table
 CREATE TABLE IF NOT EXISTS house_info (
-                         house_url VARCHAR(255) PRIMARY KEY,
+                         house_url VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY,
                          house_name text,
                          dwelling_type text,
                          -- construction_type text, (Definition unclear, seriously imbalanced)
@@ -48,7 +42,7 @@ CREATE TABLE IF NOT EXISTS house_info (
                          smoking_allowed text
 );
 
-INSERT INTO house_info
+INSERT IGNORE INTO house_info
 SELECT house_url, house_name, dwelling_type, year_of_construction, balcony, garden,
 number_of_bathrooms, number_of_bedrooms, number_of_rooms, living_area_m2, interior, energy_rating, pets_allowed, 
 smoking_allowed
@@ -68,7 +62,7 @@ WHERE dwelling_type = 'room';
 
 -- Create rental_info table
 CREATE TABLE IF NOT EXISTS rental_info (
-                         house_url VARCHAR(255),
+                         house_url VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY,
                          available DATE,
                          offered_since DATE,
                          -- minimum_months INTEGER, (82% NULL)
@@ -85,7 +79,7 @@ CREATE TABLE IF NOT EXISTS rental_info (
                          FOREIGN KEY (region) REFERENCES territory(region)
                          
 );
-INSERT INTO rental_info
+INSERT IGNORE INTO rental_info
 SELECT house_url, STR_TO_DATE(offered_since,'%d-%m-%Y') AS offered_since, STR_TO_DATE(available,'%d-%m-%Y') AS available, price,
 service_cost, status, agent_url, CONCAT(district, ', ', city) AS region
 FROM house;
@@ -99,9 +93,10 @@ SET service_cost = CASE
     ELSE 'Not Specified'
 END;
 UPDATE rental_info
-SET offered_since = current_date()
+SET offered_since = available
 WHERE offered_since IS NULL;
 SET SQL_SAFE_UPDATES = 1;
+
 
 
 
