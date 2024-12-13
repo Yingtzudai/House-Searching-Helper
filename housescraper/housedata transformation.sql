62bd3d5c6748751b0534014a68be55e853b10eb0
Ying <yingtzu212@gmail.com>
2024-12-13 16:36:18 +0100

    Started visualization

--- housescraper/housedata transformation.sql
+++ housescraper/housedata transformation.sql
@@ -14,14 +14,19 @@ FROM house;
 
 -- Create territory table
 CREATE TABLE IF NOT EXISTS territory (
+country VARCHAR(255) NOT NULL,
 city VARCHAR(255) NOT NULL,
 district VARCHAR(255) NOT NULL,
 region VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY
 );
 
-INSERT IGNORE INTO territory(city, district, region)
-SELECT city, district, CONCAT(district, ', ', city) AS region
+INSERT IGNORE INTO territory(country, city, district, region)
+SELECT city AS country, city, district, CONCAT(district, ', ', city) AS region
 FROM house;
+
+-- Transform territory table
+UPDATE territory
+SET country = "The Netherlands";
 
 -- Create house_info table
 CREATE TABLE IF NOT EXISTS house_info (
@@ -41,13 +46,17 @@ CREATE TABLE IF NOT EXISTS house_info (
                          energy_rating VARCHAR(255),
                          energy_rating_category VARCHAR(255),
                          pets_allowed text,
-                         smoking_allowed text
+                         smoking_allowed text,
+                         agent_url VARCHAR(255),
+                         region VARCHAR(255),
+						 FOREIGN KEY (agent_url) REFERENCES agent(agent_url),
+                         FOREIGN KEY (region) REFERENCES territory(region)
 );
 
 INSERT IGNORE INTO house_info
 SELECT house_url, house_name, dwelling_type, year_of_construction, balcony, garden,
 number_of_bathrooms, number_of_bedrooms, number_of_rooms, living_area_m2, interior, energy_rating, energy_rating AS energy_rating_category, pets_allowed, 
-smoking_allowed, year_of_construction AS house_age
+smoking_allowed, year_of_construction AS house_age, agent_url, CONCAT(district, ', ', city) AS region
 FROM house;
 -- Transformation for house_info
 UPDATE house_info
@@ -84,16 +93,12 @@ CREATE TABLE IF NOT EXISTS rental_info (
                          -- deposit DECIMAL, (37% NULL)
                          service_cost text,
                          status text,
-                         agent_url VARCHAR(255),
-                         region VARCHAR(255),
-                         FOREIGN KEY (agent_url) REFERENCES agent(agent_url),
-                         FOREIGN KEY (house_url) REFERENCES house_info(house_url),
-                         FOREIGN KEY (region) REFERENCES territory(region)
+                         FOREIGN KEY (house_url) REFERENCES house_info(house_url)
                          
 );
 INSERT IGNORE INTO rental_info
 SELECT house_url, STR_TO_DATE(offered_since,'%d-%m-%Y') AS offered_since, STR_TO_DATE(available,'%d-%m-%Y') AS available, price, living_area_m2 AS squared_meter_price,
-service_cost, status, agent_url, CONCAT(district, ', ', city) AS region
+service_cost, status
 FROM house;
 
 -- Transform rental_info
@@ -113,5 +118,6 @@ SET offered_since = available
 WHERE offered_since IS NULL;
 SET SQL_SAFE_UPDATES = 1;
 
-SELECT COUNT(house_url) FROM rental_info WHERE price IS NULL;
 
+SELECT DISTINCT(energy_rating_category)
+FROM house_info;
